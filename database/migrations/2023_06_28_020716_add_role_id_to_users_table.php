dce59367d3ac40ee8abde05e3150f71991183e1e
zIstarie <zIstarie@proton.me>
2023-06-27 23:59:35 -0300

    :sparkles: migrations and model creation

--- database/migrations/2023_06_28_020716_add_role_id_to_users_table.php
+++ database/migrations/2023_06_28_020716_add_role_id_to_users_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles');
         });
     }
 
@@ -26,6 +24,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
